dc0359bf1a855a6777d12fa49f391e342cf5fd8d
Akifumi1119 <akifumi.doi1119@gmail.com>
2026-05-18 21:08:07 +0900

    add lottery json

--- routes/api.php
+++ routes/api.php
@@ -15,33 +15,3 @@ Route::get('/lotteries', function () {
 
     return response()->json($lotteries);
 });
-
-Route::post('/check', function (Request $request) {
-
-    $winningNumbers = [1, 5, 12, 18, 29, 41];
-
-    $userNumbers = $request->numbers;
-
-    $matchedNumbers = array_intersect(
-        $winningNumbers,
-        $userNumbers
-    );
-
-    $matchedCount = count($matchedNumbers);
-
-    $rank = match ($matchedCount) {
-        6 => '1等',
-        5 => '2等',
-        4 => '3等',
-        3 => '4等',
-        default => 'ハズレ'
-    };
-
-    return response()->json([
-        'winning_numbers' => $winningNumbers,
-        'your_numbers' => $userNumbers,
-        'matched_numbers' => array_values($matchedNumbers),
-        'matched_count' => $matchedCount,
-        'rank' => $rank
-    ]);
-});
